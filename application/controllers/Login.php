7bd8384b6a3951d97cafd5620587dcc01eb8f66c
Jakob Grigoryan <hakob.grigoryan1992@gmail.com>
2017-10-02 12:12:21 +0400

    FIRST COMMIT

---
+++ application/controllers/Login.php
@@ -0,0 +1,80 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('admin/login.php');
+	}
+
+	public function registration(){
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('surname', 'Surname', 'required');
+        $this->form_validation->set_rules('login', 'Login', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('phone_number', 'Phone number', 'required');
+
+        if ($this->form_validation->run() === FALSE)
+        {
+//            header("Location: " . base_url() . "/index.php/login");
+            $this->load->view('admin/login');
+        }else {
+            $data = array(
+                'name' => $this->input->post('name'),
+                'surname' => $this->input->post('surname'),
+                'email' => $this->input->post('email'),
+                'login' => $this->input->post('login'),
+                'password' => $this->input->post('password'),
+                'email' => $this->input->post('email'),
+                'phone_number' => $this->input->post('phone_number')
+            );
+            $this->load->model('admins_model');
+            $this->admins_model->registration($data);
+        }
+    }
+    public function log_in(){
+        $this->load->model('admins_model');
+
+        $this->form_validation->set_rules('login', 'Login', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->load->template('login');
+        }else{
+            // $data = $this->input->post();
+
+            $data = array(
+                'Login' => $this->input->post('login'),
+                'Password' => $this->input->post('password')
+
+            );
+            $user_data = $this->admins_model->get_data([ 'id','name', 'surname', 'login','email'], $data)->row();
+            if($user_data){
+                $newdata = array(
+                    'id' => $user_data->id,
+                    'name'  => $user_data->name,
+                    'surname'   => $user_data->surname,
+                    'email' => $user_data->email,
+                    'login'  => $user_data->login,
+                );
+                $this->session->set_userdata($newdata);
+                $this->load->helper('url');
+
+                redirect(base_url().'index.php/login/home');
+
+//                header("Location: ". base_url()."index.php/login/home");
+                $this->load->template('login');
+            }
+            else{
+                redirect(base_url().'index.php/login');
+            }
+        }
+    }
+
+    public function home(){
+        $this->load->template('admin/home');
+    }
+}
+    
---
+++ application/models/admins_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Admins_model extends CI_Model {
+    protected $table = 'admins';
+
+    public function registration($data){
+
+        $this->db->insert('admins', $data);
+    }
+
+    public function get_data(array $select = ['*'], array $where){
+        return $this->db
+            ->select($select)
+            ->where($where)
+            ->get($this->table);
+    }
+}
