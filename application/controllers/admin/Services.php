521791b5c05e5332779ebdf603a561a55d4f91e5
Jakob Grigoryan <hakob.grigoryan1992@gmail.com>
2017-10-09 15:51:48 +0400

    changes

--- application/controllers/admin/Services.php
+++ application/controllers/admin/Services.php
@@ -1,6 +1,16 @@
 <?php
 class Services extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $session = $this->session->all_userdata();
+        if(!isset($session['email'])){
+            redirect(base_url().'admin/auth');
+        }
+    }
+
     public function index(){
-        
+
+        $this->load->model('services_model');
     }
 }
